fdb0bdc972df6584faa51542e62dd05b65350fc5
Shreyans Tiwari <shreyans.tiwari33@gmail.com>
2022-07-20 19:30:33 +0530

    "Solution to problem - Finding the percentage."

---
+++ finding_the_percentage.py
@@ -0,0 +1,19 @@
+"""
+The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students.
+Print the average of the marks array for the student name provided, showing 2 places after the decimal.
+"""
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+
+    s = 0
+    for i in student_marks[query_name]:
+        s += i
+
+    avg = s / len(student_marks[query_name])
+    print("{:.2f}".format(avg))
